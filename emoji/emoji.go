6c52ce7ebeeb23cccb7e532367ea2d615faaaf7b
podhmo <ababjam61+github@gmail.com>
2023-06-23 04:05:24 +0900

    feat: add emoji.Suggest()

--- emoji/emoji.go
+++ emoji/emoji.go
@@ -1,9 +1,63 @@
 package emoji
 
-import "github.com/enescakir/emoji"
+import (
+	"sort"
+	"strings"
+	"sync"
 
+	"github.com/enescakir/emoji"
+)
 
 // Trnaslate translates `:<emoji>:` to actual emoji unicode.
 func Translate(text string) string {
 	return emoji.Parse(text)
+}
+
+type SuggestOption struct {
+	Limit int
+	// TODO: order
+}
+
+type pair struct {
+	left  string
+	right string
+}
+
+var pool = &sync.Pool{
+	New: func() any {
+		source := emoji.Map()
+		r := make([]pair, len(source))
+		i := 0
+		for alias, char := range source {
+			r[i] = pair{left: alias, right: char}
+			i++
+		}
+		sort.SliceStable(r, func(i, j int) bool { return r[i].left < r[j].left })
+		return r
+	},
+}
+
+// Suggest returns the suggestions.
+func Suggest(prefix string, option SuggestOption) []string {
+	candidates := pool.Get().([]pair) // more cache?
+	defer pool.Put(candidates)
+
+	limit := option.Limit
+	var r []string
+	if limit != 0 {
+		r = make([]string, 0, limit)
+	}
+
+	// O(N) (use trie?)
+	for _, p := range candidates {
+		if !strings.HasPrefix(p.left, prefix) {
+			continue
+		}
+
+		r = append(r, p.right)
+		if limit > 0 && len(r) >= limit {
+			break
+		}
+	}
+	return r
 }
--- emoji/emoji_test.go
+++ emoji/emoji_test.go
@@ -1,6 +1,7 @@
 package emoji_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/podhmo/emoji-api/emoji"
@@ -21,6 +22,28 @@ func TestTranslate(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := emoji.Translate(tt.args.text); got != tt.want {
 				t.Errorf("Translate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSuggest(t *testing.T) {
+	type args struct {
+		prefix string
+		option emoji.SuggestOption
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{name: "simple", args: args{prefix: ":di", option: emoji.SuggestOption{}}, want: []string{"💠", "♦️", "💠", "♦️", "🇩🇬", "🔅", "🎯", "😞", "😞", "😥", "🥸", "➗", "🤿", "🪔", "💫", "😵"}},
+		{name: "simple-with-limit3", args: args{prefix: ":di", option: emoji.SuggestOption{Limit: 3}}, want: []string{"💠", "♦️", "💠"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := emoji.Suggest(tt.args.prefix, tt.args.option); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Suggest() = %v, want %v", got, tt.want)
 			}
 		})
 	}
