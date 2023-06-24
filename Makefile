57027afa45f2ca2b97ead1de98217209211f0bf3
podhmo <ababjam61+github@gmail.com>
2023-06-25 00:24:28 +0900

    gen-stub (#3)
    * chore: mv oapigen -> api/oapigen
    
    * feat: init
    
    * feat: prototype
    
    * chore: go mod tidy
    
    * fix: use named return
    
    * chore: make stub
    
    * refactor: remove debug print
    
    * refactor: remove unused code
    
    * fix: rename output -> response
    
    * chore: use gofmt
    
    * chore: make stub
    
    * fix: guess import-path for srcdir and dstdir
    
    * chore: make gen = make _gen _stub
    
    * chore: comments
    
    * refactor: remove unused code

--- Makefile
+++ Makefile
@@ -1,9 +1,20 @@
+# generate seed/design => openapi.json
 seed:
 	go run ./seed/tools/gendoc.go > openapi.json
 	go run github.com/getkin/kin-openapi/cmd/validate@latest openapi.json
 .PHONY: seed
 
 gen:
-	mkdir -p oapigen
-	go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@latest --config ./seed/tools/oapi-conf.yaml openapi.json > oapigen/server.go 
+	$(MAKE) _gen
+	$(MAKE) _stub
 .PHONY: gen
+# generate (openapi.json) => oapigen
+_gen:
+	mkdir -p api/oapigen
+	go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@latest --config ./seed/tools/oapi-conf.yaml openapi.json > api/oapigen/server.go 
+.PHONY: _gen
+# generate (openapi.json, oapigen) => api/controller
+_stub:
+	go run ./seed/tools/gen-stub --doc openapi.json --src ./api/oapigen --dst ./api/controller
+	gofmt -w ./api/controller
+.PHONY: _stub
---
+++ api/controller/emoji.go
@@ -0,0 +1,29 @@
+package controller
+
+import (
+	"context"
+
+	oapigen "github.com/podhmo/emoji-api/api/oapigen"
+)
+
+type EmojiController struct{}
+
+func NewEmojiController() *EmojiController {
+	return &EmojiController{}
+}
+
+// Suggest is endpoint of POST /emoji/suggest
+// 先頭一致で対応する文字列を探す
+//
+// * body  :requestBody                         -- "need: var body oapigen.SuggestJSONBody; gctx.ShouldBindJSON(&body); "
+func (c *EmojiController) Suggest(ctx context.Context, request oapigen.SuggestRequestObject) (response oapigen.SuggestResponseObject, err error) {
+	return
+}
+
+// Translate is endpoint of POST /emoji/translate
+// :<alias>:のような表現を含んだ文字列をemojiを使った文字列に変換する
+//
+// * body  :requestBody                         -- "need: var body oapigen.TranslateJSONBody; gctx.ShouldBindJSON(&body); "
+func (c *EmojiController) Translate(ctx context.Context, request oapigen.TranslateRequestObject) (response oapigen.TranslateResponseObject, err error) {
+	return
+}
